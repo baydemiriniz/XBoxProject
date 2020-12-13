97439d45e3280b56fb8ced17cc8a9da29b7d6fe6
baydemiriniz <73937007+baydemiriniz@users.noreply.github.com>
2020-12-13 19:53:53 +0300

    CommitToProject

---
+++ Assets/Script/GridSystem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GridSystem : MonoBehaviour
+{
+    public Text inputText;
+    public SelectButton button;
+    public GridLayoutGroup gridLayout;
+    public List<int> selectButtonInt =new List<int>();
+    private int spawnSize;
+    public void SetGrid()
+    {
+        selectButtonInt.Clear();
+        for (int i = 0; i < gridLayout.transform.childCount; i++)
+        {
+            Destroy(gridLayout.transform.GetChild(i).gameObject);
+        }//DeletedChild
+        spawnSize = Convert.ToInt32(inputText.text);
+        float cellSizeWidth = gridLayout.gameObject.GetComponent<RectTransform>().rect.width / spawnSize;
+        float cellSizeHeight = gridLayout.gameObject.GetComponent<RectTransform>().rect.height / spawnSize;
+        gridLayout.cellSize=new Vector2(cellSizeWidth,cellSizeHeight);
+        for (int i = 0; i <(spawnSize*spawnSize) ; i++)//spawnButton
+        {
+            SelectButton id= Instantiate(button, gridLayout.transform);
+            id.id = i + 1;
+            id.gridSystem = this;
+        }
+    }
+
+    public void LevelControl()
+    {
+        if (selectButtonInt.Count >= 3)
+        {
+            int id = selectButtonInt.Count - 1;
+            for (int i = 0; i < id; i++)
+            {
+                Debug.Log(selectButtonInt[id]+ "ve"+selectButtonInt[i]);
+                if ((selectButtonInt[id] - 1 == selectButtonInt[i] &&(selectButtonInt[i] % spawnSize!=0)) || //bir eksigi spawn size ye bölünmemesi gerek 
+                    ((selectButtonInt[id] + 1 == selectButtonInt[i] &&(selectButtonInt[id] % spawnSize!=0))|| //bir fazlası var ise  kendisi spawnn size ye bölünmemesi gerek 
+                     selectButtonInt[id] - spawnSize == selectButtonInt[i] ||//spawn size kadar eksiğim varmı ?
+                     selectButtonInt[id] + spawnSize == selectButtonInt[i]))//spawn size kadar fazlam varmı ? 
+                {
+                    for (int s = 0; s < id; s++)
+                    {
+                        if (selectButtonInt[i] - spawnSize == selectButtonInt[s] ||
+                            selectButtonInt[i] + spawnSize == selectButtonInt[s] ||
+                            ((selectButtonInt[i] - 1 == selectButtonInt[s]) &&(selectButtonInt[s]%spawnSize != 0 )) || 
+                            (selectButtonInt[i] + 1 == selectButtonInt[s]) &&(selectButtonInt[i]%spawnSize != 0))
+                        {
+                            SetGrid();
+                            return;
+                        }
+                        if (s != i) 
+                        {    
+                            if (selectButtonInt[id] - spawnSize == selectButtonInt[s] ||
+                                selectButtonInt[id] + spawnSize == selectButtonInt[s] ||
+                                ((selectButtonInt[id] - 1 == selectButtonInt[s]) &&(selectButtonInt[s]%spawnSize != 0 )) || 
+                                (selectButtonInt[id] + 1 == selectButtonInt[s]) &&(selectButtonInt[id]%spawnSize != 0))
+                            {
+                                Debug.Log(selectButtonInt[id]+ "ve"+selectButtonInt[s]);
+                                SetGrid();
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/SelectButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectButton : MonoBehaviour
+{
+    public int id;
+    public GridSystem gridSystem;
+
+    public void ButtonSelect()
+    {
+        gridSystem.selectButtonInt.Add(id);//id yi liste atıyoruz
+        gridSystem.LevelControl();//Methodu çalıştırıyoruz 
+    }
+}
